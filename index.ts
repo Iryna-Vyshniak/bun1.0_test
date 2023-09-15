2fd8b2d6a3e013bd420f3b38920834bba53d3a6c
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-15 18:41:47 +0300

    test

--- index.ts
+++ index.ts
@@ -1,1 +1,22 @@
-console.log("Hello via Bun!");
+import { test, expect } from 'bun:test';
+
+console.log('Hello via Bun!');
+
+const server = Bun.serve({
+  port: 3007,
+  fetch(request, server) {
+    return new Response('Hello via Bun!');
+  },
+});
+
+console.log(`Listening on ${server.port}`);
+
+const file = Bun.file('package.json');
+
+await file.text(); // string
+await file.arrayBuffer(); // ArrayBuffer
+await file.json(); // {...}
+
+test('2 + 2', () => {
+  expect(2 + 2).toBe(4);
+});

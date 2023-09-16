7c95c3a348ba62665db8e1038dd504ea1f0dcaf9
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-16 11:26:58 +0300

    serve static assets from http server

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,30 @@
 const server = Bun.serve({
-  port: 3007,
-  fetch(request, server) {
-    return new Response(
-      `
-    <html>
-    <head></head>
-    <body>
-    <h1>Hello via Bun!</h1>
-    </body>
-    </html>
-    `,
-      { headers: { 'Content-Type': 'text/html' } }
-    );
+  port: 4000,
+  async fetch(req) {
+    const url = new URL(req.url);
+
+    if (url.pathname === "/") {
+      return new Response(`
+          <html>
+            <head>
+              <script defer src="/assets/crm.js"></script>
+            </head>
+            <body>
+              <h1>Hello via Bun!</h1>
+            </body>
+          </html>
+        `, {
+        headers: {
+          "Content-Type": "text/html",
+        },
+      })
+    }
+    if (url.pathname === "/assets/crm.js") { return new Response(Bun.file('./public/crm.js')); }
+    return new Response("Page not found", { status: 404 })
   },
 });
 
-console.log(`Listening on ${server.port}`);
+console.log(`Listening on http://localhost:${server.port}`);
+
+
+
